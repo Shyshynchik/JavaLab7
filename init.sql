523902a82f20a3c7d17053075716ef7f183339ad
Игорь <igor.kostenko.1999@mail.ru>
2022-04-30 22:25:05 +0300

    added docker-compose.yml and deleted init.sql

--- init.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE table_name (
-                            id integer NOT NULL AUTO_INCREMENT,
-                            name varchar(255) NOT NULL ,
-                            PRIMARY KEY (id)
-);
