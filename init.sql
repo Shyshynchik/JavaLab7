9c10577c77e7246b10695719bf091738a1fcf4d1
Игорь <igor.kostenko.1999@mail.ru>
2022-04-30 22:24:29 +0300

    added docker-compose.yml

---
+++ init.sql
@@ -0,0 +1,5 @@
+CREATE TABLE table_name (
+                            id integer NOT NULL AUTO_INCREMENT,
+                            name varchar(255) NOT NULL ,
+                            PRIMARY KEY (id)
+);
